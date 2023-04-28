448df3f0a3a1e1e0e9db62b31b4ffa27509c17a8
Jonatan Ruben <medinaarcos.jonatanruben@gmail.com>
2023-04-27 22:00:45 -0500

    Gorillaz 27-04-2023: Gallery and function zoom

--- js/nav.js
+++ js/nav.js
@@ -4,3 +4,24 @@ let nav = document.querySelector(".nav");
 btn.addEventListener("click", function(){
     nav.classList.toggle("activo");
 });
+
+// Gallery function
+let figure_img1 = document.querySelector(".gallery-figureBox--figure-1");
+let figure_img2 = document.querySelector(".gallery-figureBox--figure-2");
+let figure_img3 = document.querySelector(".gallery-figureBox--figure-3");
+
+figure_img1.addEventListener("click", ()=>{
+    figure_img1.classList.toggle('activo');
+    figure_img2.classList.remove('activo');
+    figure_img3.classList.remove('activo');
+});
+figure_img2.addEventListener("click", ()=>{
+    figure_img1.classList.remove('activo');
+    figure_img2.classList.toggle('activo');
+    figure_img3.classList.remove('activo');
+});
+figure_img3.addEventListener("click", ()=>{
+    figure_img1.classList.remove('activo');
+    figure_img2.classList.remove('activo');
+    figure_img3.classList.toggle('activo');
+});
